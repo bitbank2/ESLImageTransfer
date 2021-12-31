494cefc7b3fac8cd2395c5ce037469a0ce1bfbc7
Laurence Bank <laurencebank@Laurences-Air.home>
2021-12-31 15:55:45 +0000

    initial commit

---
+++ app/src/main/java/com/example/eslimagetransfer/FirstFragment.java
@@ -0,0 +1,233 @@
+package com.example.eslimagetransfer;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.eslimagetransfer.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
+
+    private boolean mScanning;
+
+    private static final int RQS_ENABLE_BLUETOOTH = 1;
+
+    List<BluetoothDevice> listBluetoothDevice;
+    List<String> listBTName;
+    ListAdapter adapterLeScanResult;
+
+    private Handler mHandler;
+    private static final long SCAN_PERIOD = 5000;
+    private static Context localContext;
+    public static BluetoothDevice eslDevice;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+        binding.scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanLeDevice(true);
+            }
+        });
+
+        listBluetoothDevice = new ArrayList<>();
+        listBTName = new ArrayList<>();
+        adapterLeScanResult = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, listBTName);
+//        adapterLeScanResult = new ArrayAdapter<BluetoothDevice>(
+//                getActivity(), android.R.layout.simple_list_item_1, listBluetoothDevice);
+        binding.lelist.setAdapter(adapterLeScanResult);
+        binding.lelist.setOnItemClickListener(scanResultOnItemClickListener);
+
+        mHandler = new Handler();
+
+        localContext = getActivity().getApplicationContext();
+        // Get BluetoothAdapter and BluetoothLeScanner.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) localContext.getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+        mScanning = false;
+
+    } /* onViewCreated() */
+
+    AdapterView.OnItemClickListener scanResultOnItemClickListener =
+            new AdapterView.OnItemClickListener(){
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final BluetoothDevice device = (BluetoothDevice) listBluetoothDevice.get(position);
+                    eslDevice = device; //keep this for later
+                    String msg = device.getAddress() + "\n"
+                            + device.getBluetoothClass().toString() + "\n"
+                            + getBTDeviceType(device);
+
+                    new AlertDialog.Builder(getActivity())
+                            .setTitle(device.getName())
+                            .setMessage(msg)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .show();
+
+                }
+            };
+
+    private String getBTDeviceType(BluetoothDevice d){
+        String type = "";
+
+        switch (d.getType()){
+            case BluetoothDevice.DEVICE_TYPE_CLASSIC:
+                type = "DEVICE_TYPE_CLASSIC";
+                break;
+            case BluetoothDevice.DEVICE_TYPE_DUAL:
+                type = "DEVICE_TYPE_DUAL";
+                break;
+            case BluetoothDevice.DEVICE_TYPE_LE:
+                type = "DEVICE_TYPE_LE";
+                break;
+            case BluetoothDevice.DEVICE_TYPE_UNKNOWN:
+                type = "DEVICE_TYPE_UNKNOWN";
+                break;
+            default:
+                type = "unknown...";
+        }
+
+        return type;
+    }
+
+    /*
+     to call startScan (ScanCallback callback),
+     Requires BLUETOOTH_ADMIN permission.
+     Must hold ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION permission to get results.
+      */
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            listBluetoothDevice.clear();
+            listBTName.clear();
+            binding.lelist.invalidateViews();
+
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mBluetoothLeScanner.stopScan(scanCallback);
+                    binding.lelist.invalidateViews();
+
+                    Toast.makeText(localContext,
+                            "Scan Complete",
+                            Toast.LENGTH_LONG).show();
+
+                    mScanning = false;
+                    binding.scan.setEnabled(true);
+                }
+            }, SCAN_PERIOD);
+
+            mBluetoothLeScanner.startScan(scanCallback);
+            mScanning = true;
+            binding.scan.setEnabled(false);
+        } else {
+            mBluetoothLeScanner.stopScan(scanCallback);
+            mScanning = false;
+            binding.scan.setEnabled(true);
+        }
+    }
+
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+
+            addBluetoothDevice(result.getDevice());
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            for(ScanResult result : results){
+                addBluetoothDevice(result.getDevice());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Toast.makeText(localContext,
+                    "onScanFailed: " + String.valueOf(errorCode),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        private void addBluetoothDevice(BluetoothDevice device){
+            String deviceName = device.getName();
+            if (deviceName == null) { // use the address
+                deviceName = device.toString();
+            }
+            if(!listBluetoothDevice.contains(device)){
+                listBluetoothDevice.add(device);
+                listBTName.add(deviceName);
+                binding.lelist.invalidateViews();
+            }
+        }
+    };
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+} /* FirstFragment class */
---
+++ app/src/main/java/com/example/eslimagetransfer/SecondFragment.java
@@ -0,0 +1,168 @@
+package com.example.eslimagetransfer;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.eslimagetransfer.databinding.FragmentSecondBinding;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+    private Context localContext;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothGatt mBluetoothGatt;
+    private BluetoothGattCharacteristic mCharacteristic;
+    private BluetoothLeService mService;
+    private boolean bConnected = false;
+    private static final UUID CUSTOM_SERVICE_UUID = UUID.fromString("13187b10-eba9-a3ba-044e-83d3217d9a38");
+    private static final UUID CUSTOM_CHARACTERISTIC_UUID = UUID.fromString("4b646063-6264-f3a7-8941-e65356ea82fe");
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+        localContext = getActivity().getApplicationContext();
+
+        Toast.makeText(localContext,
+                "Connecting to ESL...",
+                Toast.LENGTH_LONG).show();
+        // Get BluetoothAdapter
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) localContext.getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        if (connectGatt(FirstFragment.eslDevice.getAddress())) {
+            mBluetoothGatt.discoverServices();
+            Toast.makeText(localContext,
+                    "Connected!",
+                    Toast.LENGTH_LONG).show();
+            bConnected = true;
+  //          mBluetoothGatt.discoverServices();
+        }
+    } /* onViewCreated() */
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+    private boolean connectGatt(final String address) {
+        if (mBluetoothAdapter == null || address == null) {
+      //      Log.w(, "BluetoothAdapter not initialized or unspecified address.");
+            return false;
+        }
+
+        if (mBluetoothGatt != null) {
+        //    Log.d(TAG, "Trying to use an existing mBluetoothGatt for connection.");
+            if (mBluetoothGatt.connect()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        final BluetoothDevice device = mBluetoothAdapter
+                .getRemoteDevice(address);
+        if (device == null) {
+       //     Log.w(TAG, "Device not found.  Unable to connect.");
+            return false;
+        }
+// NB: This won't work with LE devices unless you use the SDK 23+ version of this function
+        // and specify the transport type as LE
+        mBluetoothGatt = device.connectGatt(localContext, false, mGattCallback, BluetoothDevice.TRANSPORT_LE);
+        // Log.d(TAG, "Trying to create a new connection.");
+        return mBluetoothGatt.connect();
+    }
+    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    //bluetooth is connected so discover services
+                    bConnected = true;
+                    mBluetoothGatt.discoverServices();
+
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    //Bluetooth is disconnected
+                    bConnected = false;
+                    gatt.close();
+                }
+            } else {
+                // Error - close the connection
+                gatt.close();
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // services are discovered, find the one we want
+                final List<BluetoothGattService> services = gatt.getServices();
+                Log.i("ESLImageTransfer", String.format(Locale.ENGLISH,"discovered %d services for ESL", services.size()));
+
+            } else {
+                Log.e("ESLImageTransfer", "Service discovery failed");
+                gatt.disconnect();
+                return;
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+
+            }
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic characteristic) {
+
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+
+        }
+    };
+}
