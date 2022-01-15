6b9f002d793942cdcd1b48835eaa2fa8dcb1f77b
Laurence Bank <laurencebank@Laurences-Air.home>
2022-01-15 21:29:38 +0000

    added favorites list

--- app/src/main/java/com/example/eslimagetransfer/FirstFragment.java
+++ app/src/main/java/com/example/eslimagetransfer/FirstFragment.java
@@ -12,6 +12,7 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -30,6 +31,7 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.eslimagetransfer.databinding.FragmentFirstBinding;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,12 +47,16 @@ public class FirstFragment extends Fragment {
 
     List<BluetoothDevice> listBluetoothDevice;
     List<String> listBTName;
+    List<String> listFavoritesName;
+    List<BluetoothDevice> listFavoritesBT;
     ListAdapter adapterLeScanResult;
+    ListAdapter adapterFavorites;
 
     private Handler mHandler;
     private static final long SCAN_PERIOD = 5000;
     private static Context localContext;
     public static BluetoothDevice eslDevice;
+    public static int iSelectedPos;
 
     @Override
     public View onCreateView(
@@ -64,10 +70,62 @@ public class FirstFragment extends Fragment {
 
     }
 
+    private void ReadFavorites()
+    {
+    SharedPreferences prefs;
+        prefs =  localContext.getSharedPreferences("ESLTransfer", Context.MODE_PRIVATE);
+        int iCount = prefs.getInt("Count", 0); // how many items in the list?
+        for (int i=0; i<iCount; i++) {
+            String s = "name" + i;
+            String mac = prefs.getString(s, null);
+            BluetoothDevice btDevice = mBluetoothAdapter.getRemoteDevice(mac);
+            // Create the name from the MAC address
+            String btName = "ESL_" + mac.substring(9, 11) + mac.substring(12, 14) + mac.substring(15, 17);
+            listFavoritesBT.add(btDevice);
+            listFavoritesName.add(btName);
+        }
+        binding.favslist.invalidateViews();
+        binding.favslist.deferNotifyDataSetChanged();
+    } /* ReadFavorites() */
+
+    private void WriteFavorites()
+    {
+        SharedPreferences prefs;
+        prefs =  localContext.getSharedPreferences("ESLTransfer", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor;
+        editor = prefs.edit();
+        int iCount = listFavoritesName.size();
+        editor.clear();
+        editor.putInt("Count", iCount);
+        for (int i=0; i<iCount; i++) {
+            String s = "name" + i;
+            BluetoothDevice device = (BluetoothDevice) listFavoritesBT.get(i);
+            String mac = device.getAddress();
+            editor.putString(s, mac);
+        }
+        editor.commit();
+    } /* WriteFavorites() */
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         binding.buttonFirst.setEnabled(false);
+        binding.buttonFavorite.setEnabled(false);
+        binding.buttonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothDevice device = (BluetoothDevice) listBluetoothDevice.get(iSelectedPos);
+                if(!listFavoritesBT.contains(device)){
+                    listFavoritesBT.add(device);
+                    String name = (String)listBTName.get(iSelectedPos);
+                    listFavoritesName.add(name);
+                    binding.favslist.invalidateViews();
+                    binding.favslist.deferNotifyDataSetChanged();
+                    WriteFavorites();
+                }
+            }
+        });
+
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -94,12 +152,18 @@ public class FirstFragment extends Fragment {
 
         listBluetoothDevice = new ArrayList<>();
         listBTName = new ArrayList<>();
+        listFavoritesBT = new ArrayList<>();
+        listFavoritesName = new ArrayList<>();
+
         adapterLeScanResult = new ArrayAdapter<String>(
                 getActivity(), android.R.layout.simple_list_item_1, listBTName);
-//        adapterLeScanResult = new ArrayAdapter<BluetoothDevice>(
-//                getActivity(), android.R.layout.simple_list_item_1, listBluetoothDevice);
+        adapterFavorites = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, listFavoritesName);
+
         binding.lelist.setAdapter(adapterLeScanResult);
         binding.lelist.setOnItemClickListener(scanResultOnItemClickListener);
+        binding.favslist.setAdapter(adapterFavorites);
+        binding.favslist.setOnItemClickListener(favoritesOnItemClickListener);
 
         mHandler = new Handler();
 
@@ -112,7 +176,8 @@ public class FirstFragment extends Fragment {
             mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
 
         mScanning = false;
-
+        // Read and display the favorite devices
+        ReadFavorites();
     } /* onViewCreated() */
 
     AdapterView.OnItemClickListener scanResultOnItemClickListener =
@@ -122,6 +187,7 @@ public class FirstFragment extends Fragment {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     final BluetoothDevice device = (BluetoothDevice) listBluetoothDevice.get(position);
                     eslDevice = device; //keep this for later
+                    binding.buttonFavorite.setEnabled(true); // allow it to be added to favorites
                     if (mScanning == false)
                         binding.buttonFirst.setEnabled(true);
 //                    String msg = device.getAddress() + "\n"
@@ -138,6 +204,17 @@ public class FirstFragment extends Fragment {
 //                            })
 //                            .show();
 
+                }
+            };
+
+    AdapterView.OnItemClickListener favoritesOnItemClickListener =
+            new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final BluetoothDevice device = (BluetoothDevice) listFavoritesBT.get(position);
+                    eslDevice = device; //keep this for later
+                    iSelectedPos = position;
+                    binding.buttonFirst.setEnabled(true);
                 }
             };
 
@@ -232,7 +309,7 @@ public class FirstFragment extends Fragment {
             if (deviceName == null) { // use the address
                 deviceName = device.toString();
             }
-            if(!listBluetoothDevice.contains(device)){
+            if(deviceName.startsWith("ESL_") && !listBluetoothDevice.contains(device)){
                 listBluetoothDevice.add(device);
                 listBTName.add(deviceName);
                 binding.lelist.invalidateViews();
